7730b05ca632545defb3fe6a6ffe2c1e4f0ddc78
SMYK <samyak.sage@gmail.com>
2026-02-12 20:40:41 +0530

    add reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr;
+
+arr.push_back(0);
+arr.push_back(10);
+arr.push_back(20);
+arr.push_back(30);
+
+arr.pop_back();
+
+ reverse(arr.begin(), arr.end());
+ arr.insert(arr.end(), 10);
+for(int i = 0; i < arr.size(); i++) {
+    cout << arr[i] << " ";
+}
+
+    return 0;
+}
