1b38292fc88afd4e2a753a54a46e51547687d720
SMYK <samyak.sage@gmail.com>
2026-03-01 00:01:00 +0530

    donw

--- dsa6th/2darray/q2.cpp
+++ dsa6th/2darray/q2.cpp
@@ -12,11 +12,17 @@ using namespace std;
 //     1 2 3 4 8 12 16 15 14 13 9 5 6 7 
 
 int main() {
-    int arr[4][4] = {{1, 2, 3, 4},
-                   {5, 6, 7, 8},
-                   {9, 10, 11, 12},
-                   {13, 14, 15, 16}};
-    int t = 0, b = 3, l = 0, r = 3;
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> arr(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) { 
+            cin >> arr[i][j];
+        }
+    }
+    int t = 0, b = n-1, l = 0, r = m-1;
     
     while (t <= b && l <= r) {
         
@@ -24,12 +30,14 @@ int main() {
             cout << arr[t][i] << " ";
         }
         t++;
+        cout<<endl;
 
         
         for (int i = t; i <= b; i++) {
             cout << arr[i][r] << " ";
         }
         r--;
+        cout<<endl;
 
         
         if (t <= b) {
@@ -37,6 +45,7 @@ int main() {
                 cout << arr[b][i] << " ";
             }
             b--;
+            cout<<endl;
         }
 
        
@@ -45,17 +54,9 @@ int main() {
                 cout << arr[i][l] << " ";
             }
             l++;
+            cout<<endl;
         }
     }
 
-    // int n, m;
-    // cin >> n >> m;
-
-    // vector<vector<int>> arr(n, vector<int>(m));
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < m; j++) { 
-    //         cin >> arr[i][j];
-    //     }
-    // }
     return 0;
 }
